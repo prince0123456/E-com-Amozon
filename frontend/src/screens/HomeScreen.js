0335dbdb538bff251e0e30836bf816eb3649d196
Prince Gupta <princegupta.pg46@gmail.com>
2022-12-16 11:58:40 +0530

    fetch_api

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import data from '../data'
+// import data from '../data'
+import axios from 'axios'
 
 function HomeScreen() {
+  const [products,setProuducts]=useState([]);
+  useEffect(()=>{
+    const fetchData= async ()=>{
+      const result= await axios.get('/api/products');
+      setProuducts(result.data);
+    };
+    fetchData();
+  },[])
   return (
     <div>
           <h1>Features Products</h1>
       <div className='products'>
       {
-        data.products.map(product=>(
+        products.map((product)=>(
           <div className='product' key={product.slug}>
           <Link to={`/product/${product.slug}`}>
             <img src={product.image} alt={product.name}/>
