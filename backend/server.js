cd5a0ab3330c6bf1b00922c2842bf1098482e84e
Prince Gupta <princegupta.pg46@gmail.com>
2022-12-16 11:19:16 +0530

    create_server

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import  express  from "express";
+import data from ".data";
+
+const app=express();
+
+app.get('/api/products',(req,res)=>{
+    res.send(data.products);
+});
+
+const port=process.env.PORT || 5000;
+
+app.listen(port,()=>{
+    console.log('server is running'+port)
+})
